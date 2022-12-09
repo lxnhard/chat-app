1d11e60426a6c46ba1c713d726fb0d1d42144f42
lxnhard <106260289+lxnhard@users.noreply.github.com>
2022-12-09 15:37:20 +0100

    separate navigation component

--- App.js
+++ App.js
@@ -1,15 +1,8 @@
 import React, { Component } from 'react';
 // import react native gesture handler
 import 'react-native-gesture-handler';
-// import react Navigation
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
 // Components
-import Start from './components/Start';
-import Chat from './components/Chat';
-
-// Create the navigator
-const Stack = createStackNavigator();
+import Navigation from './components/Navigation';
 
 export default class App extends React.Component {
   constructor(props) {
@@ -18,20 +11,7 @@ export default class App extends React.Component {
 
   render() {
     return (
-      <NavigationContainer>
-        <Stack.Navigator
-          initialRouteName="Start"
-        >
-          <Stack.Screen
-            name="Start"
-            component={Start}
-          />
-          <Stack.Screen
-            name="Chat"
-            component={Chat}
-          />
-        </Stack.Navigator>
-      </NavigationContainer>
+      <Navigation></Navigation>
     );
   }
 }
