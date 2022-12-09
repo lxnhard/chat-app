3adcc81526dc0db7ba21a78c946f3ee63e706c15
lxnhard <106260289+lxnhard@users.noreply.github.com>
2022-12-09 14:30:22 +0100

    fix navigation title

--- components/Chat.js
+++ components/Chat.js
@@ -3,14 +3,15 @@ import { StyleSheet, View, Text } from 'react-native';
 
 
 export default class Chat extends React.Component {
+
   componentDidMount() {
-    let name = this.props.route.params.name;
+    let name = this.props.route.params.username;
     this.props.navigation.setOptions({ title: name });
   }
+
   render() {
     return (
       <View style={[styles.container, { backgroundColor: this.props.route.params.color }]}>
-        <Text style={styles.text}>{this.props.route.params.username}</Text>
       </View>
     )
   }
